d30e302e5a470b4c66c418f3f737694b1f845ded
Jane-lm <107050553+Jane-lm@users.noreply.github.com>
2022-09-29 11:43:40 -0500

    Login

--- projects/prueba2/src/app/app.module.ts
+++ projects/prueba2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecuperarContrasenaComponent } from './componentes/recuperar-contrasena/recuperar-contrasena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecuperarContrasenaComponent
   ],
   imports: [
     BrowserModule
