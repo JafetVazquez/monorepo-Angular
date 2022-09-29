a3f547151f77c72526f86d470e920d987ca20e23
Jafet Vazquez <jetvax.jv@gmail.com>
2022-09-29 16:45:08 -0500

    modulos/components users

--- projects/prueba2/src/app/app-routing.module.ts
+++ projects/prueba2/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { User1Component } from "./users/user1/user1.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'recuperar-contrasena',
+        component: User1Component,
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- projects/prueba2/src/app/app.module.ts
+++ projects/prueba2/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { UsersModule } from "./users/users.module";
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { User1Component } from './users/user1/user1.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    RouterModule.forRoot([
+      {path: 'user1', component: User1Component}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
