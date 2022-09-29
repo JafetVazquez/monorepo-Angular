7b95685d43e739701237826ec4b96a8e8973ad8b
Jane-lm <107050553+Jane-lm@users.noreply.github.com>
2022-09-29 16:36:40 -0500

    creacion componente login

--- projects/prueba2/src/app/app.module.ts
+++ projects/prueba2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RecuperarContrasenaComponent } from './componentes/recuperar-contrasena/recuperar-contrasena.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecuperarContrasenaComponent
+    RecuperarContrasenaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
