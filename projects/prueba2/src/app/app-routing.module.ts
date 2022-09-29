e6ca3643c7db10e9888f764a64c7dc58b6ca5efa
Jafet Vazquez <jetvax.jv@gmail.com>
2022-09-29 17:51:51 -0500

    routing

--- projects/prueba2/src/app/app-routing.module.ts
+++ projects/prueba2/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./componentes/login/login.component";
+import { User1Component } from "./users/user1/user1.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+
+    {
+        path: 'recuperar-contrasena',
+        component: User1Component
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
