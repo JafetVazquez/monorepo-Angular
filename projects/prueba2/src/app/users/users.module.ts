ff9df9b3b7a41e476aa8af8256f39876d2dccca5
Jafet Vazquez <jetvax.jv@gmail.com>
2022-10-03 16:16:44 -0500

    login v2

--- projects/prueba2/src/app/users/users.module.ts
+++ projects/prueba2/src/app/users/users.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User1Component } from './user1/user1.component';
+import { DarkModeComponent } from './dark-mode/dark-mode.component';
 
 
 
 @NgModule({
   declarations: [
-    User1Component
+    User1Component,
+    DarkModeComponent
   ],
   imports: [
     CommonModule
