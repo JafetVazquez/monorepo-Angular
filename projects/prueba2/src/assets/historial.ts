1599beb5f918a321c5282e3542dd0894734fadd1
Jane-lm <107050553+Jane-lm@users.noreply.github.com>
2022-11-09 11:14:46 -0600

    historial_ticket

---
+++ projects/prueba2/src/assets/historial.ts
@@ -0,0 +1,12 @@
+export class Historial{
+    constructor(
+        private id_historial_ticket: string,
+        private id_ticket: string,
+        private id_usuario: string,
+        private fc_modificacion: string,
+        private id_estatus: string,
+        private id_especialista: string
+
+    ){}
+    
+}
