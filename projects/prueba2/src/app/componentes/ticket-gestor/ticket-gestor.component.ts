4c847f2c680d921156b232d7d18d518749a76d3d
Jafet Vazquez <jetvax.jv@gmail.com>
2022-11-17 14:40:12 -0600

    cambio interfaz datatable

--- projects/prueba2/src/app/componentes/ticket-gestor/ticket-gestor.component.ts
+++ projects/prueba2/src/app/componentes/ticket-gestor/ticket-gestor.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { DataTablesModule } from "angular-datatables";
 import { ActivatedRoute } from "@angular/router";
 import { TicketsService } from "../../../services/tickets.service";
+import { Tickets } from "../../../assets/tickets";
+
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
   selector: 'app-ticket-gestor',
   templateUrl: './ticket-gestor.component.html',
   styleUrls: ['./ticket-gestor.component.css']
 })
-export class TicketGestorComponent implements OnInit {
+export class TicketGestorComponent implements OnInit, OnDestroy {
 
-  apiURL = 'http://localhost:3000/tickets'
+  apiURL = 'http://localhost:3000/tickets';
+  displayedColumns: string[] = ['Folio', 'Título', 'Operador', 'Fecha', 'Estatus', 'Prioridad'];
+  dataToDisplay: any = [];
   data: any;
+  showData: boolean = false;
   codigoProject = this.activatedRoute.snapshot.params['codigo']
   ticketsProject: any = {};
   nombreProject: string = '';
+
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
 
   constructor(private http: HttpClient, private activatedRoute: ActivatedRoute, private ticketsService: TicketsService) {
     this.ticketsService
@@ -39,11 +51,20 @@ export class TicketGestorComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.ticketsService.getTicketsByProject(this.codigoProject).subscribe((data: {}) => {
-    //   this.ticketsProject = data;
-    //   console.log(data);
-      
-    // });
+
+    this.http.get(this.apiURL + '?ticket_proyecto=' + this.codigoProject).subscribe((data: any) => {
+      this.data = new MatTableDataSource<Tickets>(data);
+
+      this.data.paginator = this.paginator;
+      this.data.sort = this.sort;
+    })
+  }
+
+  doFilter(value: any){
+    this.data.filter = value.trim().toLocaleLowerCase();
+  }
+
+  ngOnDestroy(): void {
     
   }
 
